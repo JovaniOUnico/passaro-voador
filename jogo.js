afd913d0628a5d29776bad5b8ce19001c825663f
jovaniAlmeida <jovani.souza.almeida@gmail.com>
2021-08-20 20:37:07 -0300

    primeiras adições cenários do jogo

--- jogo.js
+++ jogo.js
@@ -1,6 +1,16 @@
-import { Objeto, Personagem } from './objeto.js';
+import { Objeto, Personagem, Plataforma } from './objeto.js';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 console.log('[JovaniOUnico] Passaro Voador');
+
+//criando som de colisão
+const som_HIT = new Audio();
+som_HIT.src = './sons/efeitos_hit.wav';
 
 //criando imagem via javascript
 const sprite = new Image();
@@ -9,7 +19,9 @@ sprite.src = './sprites.png';
 
 //definido propriedades do canvas
 const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+var contexto = canvas.getContext('2d');
+contexto.width = canvas.width;
+contexto.height = canvas.height;
 
 const Telas = {
   inicio: {
@@ -18,6 +30,8 @@ const Telas = {
       Telas.jogo.desenha();
 
       this.msgReady.desenha(contexto, (canvas.width/2) - this.msgReady.altura / 2, 50);
+      Telas.jogo.bg.desenha(contexto);
+      Telas.jogo.chao.desenha(contexto);
 
       window.addEventListener('click', function(){
         if(Telas.telaAtiva.click){
@@ -29,35 +43,150 @@ const Telas = {
       Telas.mudaTela(Telas.jogo);
     },
     atualiza() {      
+      Telas.jogo.passaro.desenha(contexto);
+      Telas.jogo.chao.movimenta();
+      Telas.jogo.bg.movimenta();
+    },
+    inicializa() {
+      Telas.jogo.passaro = new Personagem(sprite, 33, 24, [{spriteX: 0, spriteY: 0},{spriteX: 0, spriteY: 26},{spriteX: 0, spriteY: 52},{spriteX: 0, spriteY: 26}], 10, 50);
+      Telas.jogo.chao = new Plataforma(sprite, 0, 610, 224, 112);
+      Telas.jogo.bg = new Plataforma(sprite, 390, 0, 275, 204);
+
+      //desenho do background
+      Telas.jogo.bg.desenha(contexto, 0, canvas.height - Telas.jogo.bg.altura);
+      //desenho do chão
+      Telas.jogo.chao.desenha(contexto, 0, canvas.height - Telas.jogo.chao.altura);
+      //ativa a colisão do chão
+      Telas.jogo.chao.habilitarColisao();
     }
   },
   jogo: {
-    passaro: new Personagem(sprite, 0, 0, 33, 24, 10, 50),
-    chao: new Objeto(sprite, 0, 610, 224, 112),
-    bg: new Objeto(sprite, 390,0, 275, 204),
+    passaro: new Personagem(sprite, 33, 24, [{spriteX: 0, spriteY: 0},{spriteX: 0, spriteY: 26},{spriteX: 0, spriteY: 52},{spriteX: 0, spriteY: 26}], 10, 50),
+    chao: new Plataforma(sprite, 0, 610, 224, 112),
+    bg: new Plataforma(sprite, 390, 0, 275, 204),
+
+    //canos
+    canoCeu1: new Objeto(sprite, 52, 169, 52, 400),
+    canoChao1: new Objeto(sprite, 0, 169, 52, 400),
+    canoCeu2: new Objeto(sprite, 52, 169, 52, 400),
+    canoChao2: new Objeto(sprite, 0, 169, 52, 400),
+    canoCeu3: new Objeto(sprite, 52, 169, 52, 400),
+    canoChao3: new Objeto(sprite, 0, 169, 52, 400),
+    canoCeu4: new Objeto(sprite, 52, 169, 52, 400),
+    canoChao4: new Objeto(sprite, 0, 169, 52, 400),
     desenha() {
 
       //desenho fundo
       contexto.fillStyle = '#70c5ce';
       contexto.fillRect(0, 0, canvas.width, canvas.height);
 
-      //desenho do background
-      this.bg.desenha(contexto, 0, canvas.height - this.bg.altura);
-      this.bg.desenha(contexto, this.bg.largura, canvas.height - this.bg.altura);
-
-      //desenho do chão
-      this.chao.desenha(contexto, 0, canvas.height - this.chao.altura);
-      this.chao.desenha(contexto, 0 + this.chao.largura, canvas.height - this.chao.altura);
-
       this.passaro.desenha(contexto);
+
+      //ativa a colisão do passaro
+      this.passaro.habilitarColisao();
+      this.passaro.somColisao(som_HIT);
+    },
+    click() {
+      this.passaro.pula();
     },
     atualiza() {
+      this.bg.desenha(contexto);
+      this.chao.desenha(contexto);
+      this.passaro.desenha(contexto);
+
+      //canos
+      this.canoCeu1.desenha(contexto);
+      this.canoChao1.desenha(contexto);
+      this.canoCeu2.desenha(contexto);
+      this.canoChao2.desenha(contexto);
+      this.canoCeu3.desenha(contexto);
+      this.canoChao3.desenha(contexto);
+      this.canoCeu4.desenha(contexto);
+      this.canoChao4.desenha(contexto);
+
+      if(Objeto.colisaoVerificaY(this.passaro, this.chao)){
+        setTimeout(() => {
+          Telas.mudaTela(Telas.inicio)
+        }, 500);
+        return;
+      }
+
       this.passaro.queda();
+
+      this.chao.movimenta();
+      this.bg.movimenta();
+
+      if(this.canoCeu1.x < (0 - this.canoCeu1.largura)){
+        let pos1 = getRandomInt( -this.canoCeu1.altura +100, this.chao.altura - 60);
+        this.canoCeu1.initPosY(-190 + pos1);
+        this.canoChao1.initPosY(this.canoCeu1.altura + 80 + this.canoCeu1.y);
+        this.canoCeu1.initPosX(contexto.width+ this.canoCeu1.largura + 240);
+        this.canoChao1.initPosX(contexto.width+ this.canoChao1.largura + 240);
+      }else{
+        this.canoCeu1.sumPosX(-1);
+        this.canoChao1.sumPosX(-1);
+      }
+
+      if(this.canoCeu2.x < (0 - this.canoCeu2.largura)){
+        let pos2 = getRandomInt( -this.canoCeu2.altura +100, this.chao.altura - 60);
+        this.canoCeu2.initPosY(pos2);
+        this.canoChao2.initPosY(this.canoCeu2.altura + 80 + this.canoCeu2.y);
+        this.canoCeu2.initPosX(contexto.width+ this.canoCeu2.largura + 240);
+        this.canoChao2.initPosX(contexto.width+ this.canoChao2.largura + 240);
+      }else{
+        this.canoCeu2.sumPosX(-1);
+        this.canoChao2.sumPosX(-1);
+      }
+
+      if(this.canoCeu3.x < (0 - this.canoCeu3.largura)){
+        let pos3 = getRandomInt( -this.canoCeu3.altura +100, this.chao.altura - 60);
+        this.canoCeu3.initPosY(pos3);
+        this.canoChao3.initPosY(this.canoCeu3.altura + 80 + this.canoCeu3.y);
+        this.canoCeu3.initPosX(contexto.width + this.canoCeu3.largura + 240);
+        this.canoChao3.initPosX(contexto.width + this.canoChao3.largura + 240);
+      }else{
+        this.canoCeu3.sumPosX(-1);
+        this.canoChao3.sumPosX(-1);
+      }
+
+      if(this.canoCeu4.x < (0 - this.canoCeu4.largura)){
+        let pos4 = getRandomInt( -this.canoCeu4.altura +100, this.chao.altura - 60);
+        this.canoCeu4.initPosY(pos4);
+        this.canoChao4.initPosY(this.canoCeu4.altura + 80 + this.canoCeu4.y);
+        this.canoCeu4.initPosX(contexto.width + this.canoCeu4.largura + 240);
+        this.canoChao4.initPosX(contexto.width + this.canoChao4.largura + 240);
+      }else{
+        this.canoCeu4.sumPosX(-1);
+        this.canoChao4.sumPosX(-1);
+      }
+    },
+    inicializa() {
+      let pos1 = getRandomInt( -this.canoCeu1.altura +100, this.chao.altura - 60);
+      let pos2 = getRandomInt( -this.canoCeu2.altura +100, this.chao.altura - 60);
+      let pos3 = getRandomInt( -this.canoCeu3.altura +100, this.chao.altura - 60);
+      let pos4 = getRandomInt( -this.canoCeu4.altura +100, this.chao.altura - 60);
+      this.canoCeu1.habilitarColisao();
+      this.canoCeu2.habilitarColisao();
+      this.canoCeu3.habilitarColisao();
+      this.canoCeu4.habilitarColisao();
+
+      this.canoCeu1.desenha(contexto, contexto.width + this.canoCeu1.largura + (160 * 1), -190 + pos1);
+      this.canoChao1.desenha(contexto, contexto.width + this.canoChao1.largura + (160 * 1), this.canoCeu1.altura + 80 + this.canoCeu1.y);
+
+      this.canoCeu2.desenha(contexto, contexto.width + this.canoCeu2.largura + (160 * 2), -190 + pos2);
+      this.canoChao2.desenha(contexto, contexto.width + this.canoChao2.largura + (160 * 2), this.canoCeu2.altura + 80 + this.canoCeu2.y);
+
+      this.canoCeu3.desenha(contexto, contexto.width + this.canoCeu3.largura + (160 * 3), -190 + pos3);
+      this.canoChao3.desenha(contexto, contexto.width + this.canoChao3.largura + (160 * 3), this.canoCeu3.altura + 80 + this.canoCeu3.y);
+
+      this.canoCeu4.desenha(contexto, contexto.width + this.canoCeu4.largura + (160 * 4), -190 + pos4);
+      this.canoChao4.desenha(contexto, contexto.width + this.canoChao4.largura + (160 * 4), this.canoCeu4.altura + 80 + this.canoCeu4.y);
     }
   },
   telaAtiva: {},
   mudaTela(novaTela) {
     this.telaAtiva = novaTela;
+    this.telaAtiva.inicializa();
   }
 }
 
--- objeto.js
+++ objeto.js
@@ -1,4 +1,45 @@
 class Objeto {
+
+  static colisaoVerificaY(obj1, obj2){
+    const obj1Y = obj1.y + obj1.altura;
+    const obj2Y = obj2.y;
+    
+    if(obj1.colisao == true && obj2.colisao == true){
+      if(obj1Y >= obj2Y){
+        if(obj1.colisaoSom != undefined){
+          obj1.colisaoSom.play();
+        }
+
+        if(obj2.colisaoSom != undefined){
+          obj2.colisaoSom.play();
+        }
+        return true;
+      }
+
+      return false;
+    }
+  }
+
+  static colisaoVerificaX(obj1, obj2){
+    const obj1X = obj1.x + obj1.largura;
+    const obj2X = obj2.x;
+    
+    if(obj1.colisao == true && obj2.colisao == true){
+      if(obj1X >= obj2X){
+        if(obj1.colisaoSom != undefined){
+          obj1.colisaoSom.play();
+        }
+
+        if(obj2.colisaoSom != undefined){
+          obj2.colisaoSom.play();
+        }
+        return true;
+      }
+
+      return false;
+    }
+  }
+
   constructor (sprite, dx, dy, largura, altura) {
     //imagem para pegar o objeto como base
     this.sprite = sprite;
@@ -50,24 +91,138 @@ class Objeto {
       this.x, this.y,
       this.largura, this.altura
     );
+
+  }
+
+  habilitarColisao(){
+    this.colisao = true;
+  }
+
+  somColisao(aud) {
+    this.colisaoSom = aud;
+  }
+
+  movimenta(){
+    let movimentoChao = 1;
+    let repeteEm = this.largura / 2;
+    let movimentacao = this.x - movimentoChao;
+
+    this.x = movimentacao % repeteEm;
   }
 }
 
 class Personagem extends Objeto {
-  constructor (sprite, dx, dy, largura, altura, px, py) {
-    super(sprite, dx, dy, largura, altura);
+
+  static frameAtual = 0;
+
+  constructor (sprite, largura, altura, spritesPos, px, py) {
+    super();
+
+    this.sprite = sprite;
 
     this.gravidade = 0.25;
     this.velocidade = 0;
+    this.pulo = 4.6;
+
+    this.spritesPos = spritesPos;
+    this.posSprite = 0;
 
     this.x = px;
     this.y = py;
-  }
 
+    //tamanho do recorte
+    this.altura = altura;
+    this.largura = largura;
+
+  }
+  pula(){
+    this.velocidade = - this.pulo;
+  }
   queda(){
     this.velocidade = this.velocidade + this.gravidade;
     this.y += this.velocidade;
   }
+  desenha(contexto, x, y){
+    //posição no canvas
+    if(x != undefined){
+      this.x = x;
+    }
+
+    if(y != undefined){
+      this.y = y;
+    }
+
+
+    let spx = this.spritesPos[this.posSprite].spriteX;
+    let spy = this.spritesPos[this.posSprite].spriteY;
+
+    //vai desenhar uma imagem no canvas
+    contexto.drawImage(
+      //essa é a imagem base
+      this.sprite,
+      //essas são as posições da imagem que voce quer cortar apartir da imagem base
+      spx, spy,
+      //esses são os tamanhos do corte da imagem recortada
+      this.largura, this.altura,
+      this.x, this.y,
+      this.largura, this.altura
+    );
+
+    this.atualizaFrameAtual();
+    Personagem.frameAtual++;
+  }
+  atualizaFrameAtual(){
+    const intervaloFrames = 10;
+    const passouIntervalo = Personagem.frameAtual % intervaloFrames === 0;
+
+    if(passouIntervalo){
+      if(this.posSprite < (this.spritesPos.length-1)){
+        this.posSprite++;
+      }else{
+        this.posSprite = 0;
+      }
+    }
+  }
 }
 
-export {Objeto, Personagem}
+class Plataforma extends Objeto{
+  desenha(contexto, x, y){
+    //posição no canvas
+    if(x != undefined){
+      this.x = x;
+    }
+
+    if(y != undefined){
+      this.y = y;
+    }
+
+    /* intenção é fazer repetir a plataforma 2 vezes */
+
+    //vai desenhar uma imagem no canvas
+    contexto.drawImage(
+      //essa é a imagem base
+      this.sprite,
+      //essas são as posições da imagem que voce quer cortar apartir da imagem base
+      this.spriteX, this.spriteY,
+      //esses são os tamanhos do corte da imagem recortada
+      this.largura, this.altura,
+      this.x, this.y,
+      this.largura, this.altura
+    );
+
+    //vai desenhar a segunda imagem no canvas
+    contexto.drawImage(
+      //essa é a imagem base
+      this.sprite,
+      //essas são as posições da imagem que voce quer cortar apartir da imagem base
+      this.spriteX, this.spriteY,
+      //esses são os tamanhos do corte da imagem recortada
+      this.largura, this.altura,
+      this.largura + this.x, this.y,
+      this.largura, this.altura
+    );
+
+  }
+}
+
+export {Objeto, Personagem, Plataforma}
